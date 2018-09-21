57bd364b60f780bbe2c42ef20e5131435a7e9307
Michael Rivera <soymrmike@gmail.com>
2018-09-20 20:21:45 -0700

    can capture screen and insert it into a video tag.

--- index.js
+++ index.js
@@ -4,7 +4,41 @@ window.eval = global.eval = function () {
   throw new Error(`Sorry, this app does not support window.eval().`);
 };
 
+const { desktopCapturer } = require('electron');
+
 document.addEventListener('DOMContentLoaded', () => {
+
+  desktopCapturer.getSources({types: ['screen']}, (error, sources) => {
+    if(error) throw error;
+
+    for (let i = 0; i < sources.length; ++i) {
+      console.log(sources[i]);
+      if (i == 0) {
+        navigator.mediaDevices.getUserMedia({
+          audio: false,
+          video: {
+            mandatory: {
+              chromeMediaSource: 'desktop',
+              chromeMediaSourceId: sources[i].id
+            }
+          }
+        }).then((stream) => handleStream(stream))
+        .catch((e) => handleError(e));
+
+        return;
+      }
+    }
+  });
+
+  function handleStream(stream) {
+    const video = document.querySelector('video');
+    video.srcObject = stream;
+    video.onloadedmetadata = (e) => video.play();
+  }
+
+  function handleError(e) {
+    console.log(e);
+  }
 
   document.querySelector('.newAgentForm').addEventListener('submit', (e) => {
 
